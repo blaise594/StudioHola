5d047a1157ba4175daf5bdb4b9d5575d219f5eeb
Daniel <danielbrogers594@gmail.com>
2018-02-13 20:47:26 -0500

    first commit

---
+++ StudioHola/Controllers/AnotherController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudioHola.Controllers
+{
+    public class AnotherController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<select name='language'><option value='english' selected>English</option>" +
+                "<option value='spanish'>Spanish</spanish>" +
+                "<option value='french'>French</french>" +
+                "<option value='italian'>Italian</italian>" +
+                "<option value='german'>German</german>" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+        [Route("/Another")]
+        [HttpPost]
+        public IActionResult Display(string name = "world", string language = "English")
+        {
+            return Content(CreateMessage(name, language));
+        }
+        public static string CreateMessage(string name, string language)
+        {
+            string helloTranslation = "";
+            switch (language)
+            {
+                case "spanish":
+                    helloTranslation = "Hola ";
+                    break;
+                case "french":
+                    helloTranslation = "Bonjour ";
+                    break;
+                case "italian":
+                    helloTranslation = "Ciao ";
+                    break;
+                case "german":
+                    helloTranslation = "Hallo ";
+                    break;
+
+            }
+            return helloTranslation + name;
+        }
+    }
+}
